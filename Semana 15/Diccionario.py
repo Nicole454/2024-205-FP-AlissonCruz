cf0ca087604f8408f5fefcbbfc3ab889aa000a8f
Cruz Borja Alisson Nicole <an.cruzb@uea.edu.ec>
2025-03-27 18:09:47 -0500

    Semana 15

---
+++ Semana 15/Diccionario.py
@@ -0,0 +1,13 @@
+# Diccionario informacion_personal
+
+diccionario = {"Nombre":"Alisson","Edad":"18","Ciudad":"Quito","Profesion":"SISTEMAS"}
+
+diccionario["Ciudad"] = "Cuenca"
+
+diccionario["Profesion"] = "Ingeniera en sistemas "
+
+diccionario["Telefono "] = "0990877562"
+
+del(diccionario["Edad"])
+
+print(diccionario)
